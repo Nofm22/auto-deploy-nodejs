9539e3a1c0887b615e69f8977b0ecaa140c74d5d
NhatMinh <minhphan113@gmail.com>
2023-02-22 21:58:08 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express=require('express');
-var app=express();
+var express = require("express");
+var app = express();
 
-var routes=require('./routes/route.js');
+var routes = require("./routes/route.js");
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',routes.home);
+app.get("/", (req, res) => {
+  res.send("Hello tự deploy được rồi nè mà không biết sao nó chạy được lun á");
+});
 
 var port = process.env.PORT || 3000;
 
-var server=app.listen(port,function(req,res){
-    console.log("Catch the action at http://localhost:"+port);
+var server = app.listen(port, function (req, res) {
+  console.log("Catch the action at http://localhost:" + port);
 });
