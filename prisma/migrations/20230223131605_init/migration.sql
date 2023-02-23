55bdbfc43a06f55e8e295e2409707b9c78e21b16
NhatMinh <minhphan113@gmail.com>
2023-02-23 20:27:54 +0700

    Add migration

---
+++ prisma/migrations/20230223131605_init/migration.sql
@@ -0,0 +1,124 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NULL,
+    `password` VARCHAR(255) NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+    `role` VARCHAR(255) NOT NULL DEFAULT 'Teacher',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `classes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `gradeId` INTEGER NULL,
+    `name` VARCHAR(255) NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+    `numberStudent` INTEGER NOT NULL,
+
+    INDEX `gradeId`(`gradeId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `courses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `gradeId` INTEGER NULL,
+    `name` VARCHAR(255) NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+    `passScore` FLOAT NULL DEFAULT 5,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `grades` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+    `minOld` INTEGER NULL,
+    `maxOld` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `scores` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `courseId` INTEGER NULL,
+    `exam15` FLOAT NULL,
+    `exam45` FLOAT NULL,
+    `examFinal` FLOAT NULL,
+    `studentId` INTEGER NULL,
+    `semesterOne` BOOLEAN NOT NULL,
+    `semesterTwo` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+
+    INDEX `courseId`(`courseId`),
+    INDEX `studentId`(`studentId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sequelizemeta` (
+    `name` VARCHAR(255) NOT NULL,
+
+    UNIQUE INDEX `name`(`name`),
+    PRIMARY KEY (`name`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `students` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `gender` BOOLEAN NULL,
+    `date` DATETIME(0) NULL,
+    `address` VARCHAR(255) NULL,
+    `email` VARCHAR(255) NULL,
+    `classId` INTEGER NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+
+    INDEX `classId`(`classId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `teachers` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `gender` BOOLEAN NULL,
+    `date` DATETIME(0) NULL,
+    `address` VARCHAR(255) NULL,
+    `email` VARCHAR(255) NULL,
+    `classId` INTEGER NULL,
+    `createdAt` DATETIME(0) NOT NULL,
+    `updatedAt` DATETIME(0) NOT NULL,
+    `courseId` INTEGER NULL,
+    `userId` INTEGER NULL,
+    `gradeId` INTEGER NULL,
+
+    INDEX `classId`(`classId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `classes` ADD CONSTRAINT `Classes_ibfk_1` FOREIGN KEY (`gradeId`) REFERENCES `grades`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `scores` ADD CONSTRAINT `scores_ibfk_1` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `scores` ADD CONSTRAINT `scores_ibfk_2` FOREIGN KEY (`studentId`) REFERENCES `students`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `students` ADD CONSTRAINT `Students_ibfk_1` FOREIGN KEY (`classId`) REFERENCES `classes`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `teachers` ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`classId`) REFERENCES `classes`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
---
+++ prisma/migrations/20230223132429_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `is_pass` BOOLEAN NULL;
