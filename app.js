8f2a971a55388ac8dcfb5cd4687ed1bb1bb11c02
NhatMinh <minhphan113@gmail.com>
2023-02-23 22:15:31 +0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require("express");
 var app = express();
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient();
 var routes = require("./routes/route.js");
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello from ptnminh with love");
+app.get("/", async (req, res) => {
+  const newUser = await prisma.users.create({
+    data: {
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },
+  });
+
+  const users = await prisma.users.findMany();
+  res.send(users);
 });
 
 var port = process.env.PORT || 3000;
